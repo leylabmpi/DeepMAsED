14daf5f1d383da328f7883ef3876416b37ed61d3
Mateo Rojas-Carulla <mrojas@login2.is.localnet>
2019-06-19 17:03:42 +0200

    Added data generator

--- dl_code/models.py
+++ dl_code/models.py
@@ -1,3 +1,4 @@
+import numpy as np
 import keras
 from keras.models import Model, Sequential
 from keras.layers import Input, LSTM, Dense, BatchNormalization, AveragePooling2D
@@ -76,3 +77,72 @@ class chimera_net(object):
 
     def save(self, path):
         self.net.save(path)
+
+
+class Generator(keras.utils.Sequence):
+    def __init__(self, x, y, max_len, batch_size=32, shuffle=True): 
+        'Initialization'
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.max_len = max_len
+        self.x = x
+        self.y = y
+        self.shuffle = shuffle
+        self.n_feat = x[0].shape[1]
+
+        mean, std = utils.compute_mean_std(self.x)
+        self.mean = mean
+        self.std = std
+
+        # Shuffle data
+        self.indices = np.arange(len(x))
+        if self.shuffle: 
+            np.random.shuffle(self.indices)
+
+        self.on_epoch_end()
+
+    def on_epoch_end(self):
+        """
+        Reshuffle when epoch ends 
+        """
+        if self.shuffle: 
+            np.random.shuffle(self.indices)
+
+
+    def generate(self, indices_tmp):
+        """
+        Generate new mini-batch
+        """
+
+        x_mb = np.zeros((self.batch_size, self.max_len, self.n_feat, 1))
+        y_mb = np.zeros((self.batch_size, 1))
+
+        for i, idx in enumerate(indices_tmp):
+            x_mb[i, 0:self.x[idx].shape[0]] = np.expand_dims(
+              (self.x[idx] - self.mean) / self.std, -1)
+            y_mb[i] = self.y[idx]
+
+        return x_mb, y_mb
+
+    def __len__(self):
+        return int(np.floor(len(self.indices) / self.batch_size))
+
+    def __getitem__(self, index):
+        """
+        Get new mb
+        """
+        indices_tmp = \
+          self.indices[self.batch_size * index : self.batch_size * (index + 1)]
+        x_mb, y_mb = self.generate(indices_tmp)
+        return x_mb, y_mb
+
+
+
+            
+
+
+
+
+
+
+
