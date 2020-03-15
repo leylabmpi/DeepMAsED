a305c4f1a25fd87c61e6f24c956951b5879475ce
Nicholas Youngblut <nicholas.youngblut@tuebingen.mpg.de>
2020-03-15 10:56:57 +0100

    added skipped; MGSIM=0.1.9; added read_n6_dl

---
+++ DeepMAsED-SM/tests/data/read_n6_dl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+wget http://ftp.tue.mpg.de/ebio/nyoungblut/mg_read_data/samp1_R1.fq.gz
+wget http://ftp.tue.mpg.de/ebio/nyoungblut/mg_read_data/samp1_R2.fq.gz
+wget http://ftp.tue.mpg.de/ebio/nyoungblut/mg_read_data/samp2_R1.fq.gz
+wget http://ftp.tue.mpg.de/ebio/nyoungblut/mg_read_data/samp2_R2.fq.gz
+wget http://ftp.tue.mpg.de/ebio/nyoungblut/mg_read_data/samp3_R1.fq.gz
+wget http://ftp.tue.mpg.de/ebio/nyoungblut/mg_read_data/samp3_R2.fq.gz
+wget http://ftp.tue.mpg.de/ebio/nyoungblut/mg_read_data/samp4_R1.fq.gz
+wget http://ftp.tue.mpg.de/ebio/nyoungblut/mg_read_data/samp4_R2.fq.gz
+wget http://ftp.tue.mpg.de/ebio/nyoungblut/mg_read_data/samp5_R1.fq.gz
+wget http://ftp.tue.mpg.de/ebio/nyoungblut/mg_read_data/samp5_R2.fq.gz
+wget http://ftp.tue.mpg.de/ebio/nyoungblut/mg_read_data/samp6_R1.fq.gz
+wget http://ftp.tue.mpg.de/ebio/nyoungblut/mg_read_data/samp6_R2.fq.gz
